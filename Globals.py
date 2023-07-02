56ee301ca5764dc214c7ebe445483101b3b992ef
Vladislav <v.v.somov.71@gmail.com>
2023-07-02 16:12:59 +0300

    Add Dockerfile, Add requests to requirements
    Docker file working on setting environment variables (example can be found in Globals.env.sample)
    Dockerized Globals.py extracts values from os.environ
    Not great, but solid working

--- Globals.py
+++ Globals.py
@@ -1,15 +1,16 @@
+import os
 #strings
 
-DAVINCI_TOKEN = [Token of Discord bot]
+DAVINCI_TOKEN = os.environ["MJW_DAVINCI_TOKEN"]
 
-SERVER_ID = [Server id here]
+SERVER_ID = os.environ["MJW_SERVER_ID"]
 
-SALAI_TOKEN = [Token of the Account from which you paid MidJourney ]
+SALAI_TOKEN = os.environ["MJW_SALAI_TOKEN"]
 
-CHANNEL_ID = [Channel in which commands are sent]
+CHANNEL_ID = os.environ["MJW_CHANNEL_ID"]
 
 #boolean
-USE_MESSAGED_CHANNEL = False
+USE_MESSAGED_CHANNEL = bool(os.environ["MJW_USE_MESSAGED_CHANNEL"])
 
 #don't edit the following variable
 MID_JOURNEY_ID = "936929561302675456"  #midjourney bot id
