5989e811cf16c04349ebd2ce50aae0f4a45e60cd
Vladislav <v.v.somov.71@gmail.com>
2023-07-02 16:57:05 +0300

    Update Docker to use 3.11.4, Update requirements to use pycord v2.4, Add suffer fix to version

--- Salai.py
+++ Salai.py
@@ -4,10 +4,10 @@ import requests
 def PassPromptToSelfBot(prompt : str):
     payload ={"type":2,"application_id":"936929561302675456","guild_id":Globals.SERVER_ID,
               "channel_id":Globals.CHANNEL_ID,"session_id":"2fb980f65e5c9a77c96ca01f2c242cf6",
-              "data":{"version":"1077969938624553050","id":"938956540159881230","name":"imagine","type":1,"options":[{"type":3,"name":"prompt","value":prompt}],
+              "data":{"version":"1118961510123847772","id":"938956540159881230","name":"imagine","type":1,"options":[{"type":3,"name":"prompt","value":prompt}],
                       "application_command":{"id":"938956540159881230",
                                              "application_id":"936929561302675456",
-                                             "version":"1077969938624553050",
+                                             "version":"1118961510123847772",
                                              "default_permission":True,
                                              "default_member_permissions":None,
                                              "type":1,"nsfw":False,"name":"imagine","description":"Create images with Midjourney",
